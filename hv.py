32999a3f25f31c7ecb5f0f991bd9d8abbfa072ee
lesha-akim <162914737+lesha-akim@users.noreply.github.com>
2024-03-10 21:35:47 +0300

    Update hv.py

--- hv.py
+++ hv.py
@@ -1,16 +1,19 @@
+#импортируем библиотеки нумпаи и пандас
 import pandas as pd
-import numpy as np
+import numpy as np        
 
-
+#создаем таблицу умножения
 lst = dict()
 for i in range(1, 10):
     lst.update({i: [i*j for j in range(1, 10)]})
 tabl_umn = pd.DataFrame(lst, index=np.arange(1, 10))
 
+#создаем таблицу сложения
 lst2 = dict()
 for i in range(1, 10):
     lst2.update({i: [i+j for j in range(1, 10)]})
 tabl_slozh = pd.DataFrame(lst2, index=np.arange(1, 10))
 
+#выводим таблицы
 print(f"Таблица умножения \n{tabl_umn}\n")
 print(f"Таблица сложения \n{tabl_slozh}")
